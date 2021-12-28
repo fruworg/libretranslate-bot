f590cd0f6519e18f76e7633357ee84b89e11260d
Mitch Conner <96399261+fruworg@users.noreply.github.com>
2021-12-28 20:52:42 +0500

    Update main.go

--- main.go
+++ main.go
@@ -14,20 +14,33 @@ import (
 )
 
 func main() {
-	// токен
+		// токен
 	bot, err := tgbotapi.NewBotAPI("telegramToken")
 	if err != nil {
-		log.Print(err)
+		log.Fatal(err)
 	}
+ 
+	bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	// u - апдейты
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	wh, _ := tgbotapi.NewWebhookWithCert("https://libretranslate-bot.herokuapp.com", ":" + "PORT")
 
-	// новые сообщения
-	updates, err := bot.GetUpdatesChan(u)
+	_, err = bot.SetWebhook(wh)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if info.LastErrorDate != 0 {
+		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+	}
+
+	updates := bot.ListenForWebhook("/" + bot.Token)
 
 	// обрабатываем
 	for update := range updates {
