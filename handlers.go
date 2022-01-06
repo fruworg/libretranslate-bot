665499775ccf72ae9687615f7e104c367bd7a862
Mitch Conner <96399261+fruworg@users.noreply.github.com>
2022-01-06 05:28:30 +0500

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -109,7 +109,7 @@ func (a *application) msgHandler(m *tbot.Message) {
 			log.Println(err)
 		}
 
-		resp, err := http.Post("https://libretranslate.de/translate", "application/json",
+		resp, err := http.Post("https://translate.argosopentech.com/", "application/json",
 			bytes.NewBuffer(bytesRepresentation))
 		if err != nil {
 			log.Println(err)
